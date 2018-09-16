98ff35510e113afd351ba12922bd6cbfdce64341
krircc <krircc@aliyun.com>
2018-09-16 09:52:16 +0800

    blog

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ let { blog } = require ('./category/blog.js')
 
 module.exports = {
     title: 'Tokio中文',
-    description: 'Tokio：Rust编写快速网络应用的平台',
+    description: 'Tokio：Rust编程语言的异步运行时,提供异步事件驱动平台，构建快速，可靠和轻量级网络应用。利用Rust的所有权和并发模型确保线程安全',
     head: [
       ['link', { rel: 'icon', href: `/favicon.ico` }],
       ['link', { rel: 'manifest', href: '/manifest.json' }],
